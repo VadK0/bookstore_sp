d7b10eb30c32e6aa1def54151a7a6ebd0fb1603f
unknown <7096544@gmail.com>
2020-09-22 16:29:01 -0700

    Very First Commit

---
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -0,0 +1,10 @@
+package com.example.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	@GetMapping("/index")
+	public String function() {return "";}
+}
