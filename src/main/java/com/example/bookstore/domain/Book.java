853a0d840d7916c3aa475f55ed13819324a54533
unknown <7096544@gmail.com>
2020-10-07 20:39:48 -0700

    REST

--- src/main/java/com/example/bookstore/domain/Book.java
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Book {
 	@Id
@@ -21,6 +23,7 @@ public class Book {
 	
 	@ManyToOne
     @JoinColumn(name = "cattid")
+	@JsonBackReference
 	private Category category;
 	
 	public Book() {}
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,18 +1,19 @@
 package com.example.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.CategoryRepository;
-
-
-
 
 @Controller
 public class BookController {
@@ -27,6 +28,17 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+	
+	 @RequestMapping(value="/books", method = RequestMethod.GET)
+	 public @ResponseBody List<Book> bookListRest() {	
+		 return (List<Book>) repository.findAll();
+	 }
+	 
+	 @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	 public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+		 return repository.findById(bookId);
+	 }
+	
 	
 	@RequestMapping(value = "/add")
     public String addBook(Model model){
