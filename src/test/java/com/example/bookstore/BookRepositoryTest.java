70e398c1846576ab7f09c1d510fa6ef0b1a9e2cb
Vadim K <34627823+VadK0@users.noreply.github.com>
2020-12-07 07:41:15 +0300

    Deployed (#1)
    * Update pom.xml
    
    * Update application.properties
    
    * 111
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Create system.properties
    
    * Update pom.xml
    
    * Update pom.xml
    
    * 2222
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * 333
    
    * Update pom.xml
    
    * 444
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * 555
    
    * 555
    
    * Update BookRepositoryTest.java
    
    * 000

--- src/test/java/com/example/bookstore/BookRepositoryTest.java
+++ src/test/java/com/example/bookstore/BookRepositoryTest.java
@@ -4,11 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
@@ -16,7 +17,7 @@ import com.example.bookstore.domain.Category;
 
 
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @DataJpaTest
 public class BookRepositoryTest {
 	@Autowired
--- src/test/java/com/example/bookstore/UserRepositoryTest.java
+++ src/test/java/com/example/bookstore/UserRepositoryTest.java
@@ -2,16 +2,17 @@ package com.example.bookstore;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.bookstore.domain.User;
 import com.example.bookstore.domain.UserRepository;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserRepositoryTest {
 	@Autowired
